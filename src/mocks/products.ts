4e1afb2ee0120cdcba1c214a7546b7534e0f3c14
mateusfelixdias <felixmateus618@gmail.com>
2023-05-24 16:20:42 -0300

    feat: added data for test

---
+++ src/mocks/products.ts
@@ -0,0 +1,39 @@
+export const products = [
+  {
+    name: 'Pizza quatro queijos',
+    _id: '6372e040f52e37ef85fe2c5e',
+    imagePath: '1670513137538-quatro-queijos.png',
+    description: 'Deliciosa pizza quatro queijos com borda simples',
+    price: 40,
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c5f',
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c60',
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c61',
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c62',
+      },
+    ],
+  },
+  {
+    price: 7,
+    ingredients: [],
+    name: 'Coca cola',
+    _id: '6372e276a381106c0f854cb3',
+    imagePath: '1670706102857-coca-cola.png',
+    description: 'Coca cola lata geladinha topzera',
+  },
+];
