6da06cb8c7c513ba288817fce1ad42f015a944b8
mateusfelixdias <felixmateus618@gmail.com>
2023-05-24 15:07:48 -0300

    chore: added fonts config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
+import { Main } from './src/Main';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+  });
+
+  if (!isFontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+
+      <Main />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
